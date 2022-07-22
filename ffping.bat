fa43a4e16b43b4783ad9079b474dfed582f14123
Harya, Henry <henry.harya@wabtec.com>
2022-07-22 14:12:46 +0200

    reorganize

---
+++ ffping.bat
@@ -0,0 +1,18 @@
+rem Japan: 124.150.157.15 (traceroute 124.150.157.15)
+ping 124.150.157.15
+rem North America: 204.2.229.116 (traceroute 204.2.229.116)
+ping 204.2.229.116 
+rem Europe: 195.82.50.33 (traceroute 195.82.50.33)
+ping 195.82.50.33 
+rem Oceania: 153.254.80.65 (traceroute 153.254.80.65)
+ping 153.254.80.65 
+
+
+rem Japan: 124.150.157.15 (traceroute 124.150.157.15)
+tracert 124.150.157.15
+rem North America: 204.2.229.116 (traceroute 204.2.229.116)
+tracert 204.2.229.116 
+rem Europe: 195.82.50.33 (traceroute 195.82.50.33)
+tracert 195.82.50.33 
+rem Oceania: 153.254.80.65 (traceroute 153.254.80.65)
+tracert 153.254.80.65 
